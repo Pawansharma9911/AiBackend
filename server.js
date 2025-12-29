f75b9483dcc815f8ace90eb45e4bd1189f3cbc12
Pawansharma9911 <124700568+Pawansharma9911@users.noreply.github.com>
2025-12-29 23:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,26 +43,6 @@ connectDB();
 
 
 
-const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
-});
 
-app.post("/test", async (req, res) => {
-  try {
-    const completion = await groq.chat.completions.create({
-      model: "llama-3.1-8b-instant",
-      messages: [
-        {
-          role: "user",
-          content: req.body.message,
-        },
-      ],
-    });
-    res.send(completion.choices[0].message.content);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Groq API error");
-  }
-});
 export default app;
 
