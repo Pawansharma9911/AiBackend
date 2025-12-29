6410afa8721e0a20158d6d0b783e0ea9d161bfe8
Pawansharma9911 <124700568+Pawansharma9911@users.noreply.github.com>
2025-12-29 22:44:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-frontend-cyan-two.vercel.app"
+  ],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true
+}));
 
 app.use("/api", chatRoutes);
 
